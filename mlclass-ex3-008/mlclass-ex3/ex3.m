b17441f09b51943102d1d7504421bc470659406b
yaroslav.yermilov <yermilov17@gmail.com>
2015-03-06 14:52:57 +0200

    do not plot the data

--- mlclass-ex3-008/mlclass-ex3/ex3.m
+++ mlclass-ex3-008/mlclass-ex3/ex3.m
@@ -35,15 +35,6 @@ fprintf('Loading and Visualizing Data ...\n')
 load('ex3data1.mat'); % training data stored in arrays X, y
 m = size(X, 1);
 
-% Randomly select 100 data points to display
-rand_indices = randperm(m);
-sel = X(rand_indices(1:100), :);
-
-displayData(sel);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
 %% ============ Part 2: Vectorize Logistic Regression ============
 %  In this part of the exercise, you will reuse your logistic regression
 %  code from the last exercise. You task here is to make sure that your
