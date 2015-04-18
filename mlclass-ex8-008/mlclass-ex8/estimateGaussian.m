768b4712579ac00354692924410cd4351cca7fef
yaroslav.yermilov <yermilov17@gmail.com>
2015-04-18 15:18:56 +0300

    assignment #8 task #1

--- mlclass-ex8-008/mlclass-ex8/estimateGaussian.m
+++ mlclass-ex8-008/mlclass-ex8/estimateGaussian.m
@@ -22,12 +22,19 @@ sigma2 = zeros(n, 1);
 %
 
 
+for i = 1:n 
+	for j = 1:m
+		mu(i) = mu(i) + X(j, i);
+	end
+	mu(i) = mu(i) / m;
+end
 
-
-
-
-
-
+for i = 1:n 
+	for j = 1:m
+		sigma2(i) = sigma2(i) + (X(j, i) - mu(i)) ^ 2;
+	end
+	sigma2(i) = sigma2(i) / m;
+end
 
 
 % =============================================================
